dcd3f095f40ff515fea728d7c65e4b6bdf8e641f
typosbro <typosbro@proton.me>
2025-06-04 14:05:17 +0900

    publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,27 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") version "2.0.0"
+    `java-library`
+    `maven-publish`
 }
 
 group = "io.github.typosbro"
-version = "1.0-SNAPSHOT"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
 }
 
 dependencies {
@@ -18,6 +33,49 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+
 kotlin {
-    jvmToolchain(23)
+    jvmToolchain(11)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            pom {
+                name.set("Kotlin Num2Words Library")
+                description.set("A Kotlin library to convert numbers to words.")
+                url.set("https://github.com/typosbro/num2words")
+                 licenses {
+                     license {
+                         name.set("The Apache License, Version 2.0")
+                         url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                     }
+                 }
+                 developers {
+                     developer {
+                         id.set("typosbro")
+                         name.set("Typosbro")
+                         email.set("typosbro@proton.me")
+                     }
+                 }
+                 scm {
+                     connection.set("scm:git:git://github.com/typosbro/num2words.git")
+                     developerConnection.set("scm:git:ssh://github.com/typosbro/num2words.git")
+                     url.set("https://github.com/typosbro/num2words/tree/main")
+                 }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/typosbro/num2words")
+            credentials {
+                username = System.getenv("GITHUB_USER") ?: project.findProperty("gpr.user")?.toString()
+                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.key")?.toString()
+            }
+        }
+    }
 }
